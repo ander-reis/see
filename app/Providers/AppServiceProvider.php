27331ccf320ec28263e0a8e47e08563a46863a78
Anderson <anderson@sinprosp.org.br>
2019-03-18 14:07:14 -0300

    Alterado pasta do projeto Instalado IDE helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        /**
+         * Configuração IDE Helper
+         */
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+
+        /**
+         * Registra faker com a linguagem em portugues pt_BR
+         */
+        $this->app->extend(\Faker\Generator::class, function (){
+            return \Faker\Factory::create('pt_BR');
+        });
     }
 
     /**
--- database/migrations/2014_10_12_000000_create_tb_see_users_table.php
+++ database/migrations/2014_10_12_000000_create_tb_see_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class TbSeeUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tb_see_users', function (Blueprint $table) {
             $table->increments('id');
             //$table->string('name');
             $table->string('username')->unique();
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tb_see_users');
     }
 }
